8fafb1c265c3e1d4f78eeed5ae111a8f7efbf9d5
Iván Liszewski <ivan@lisz.org>
2023-08-13 22:51:04 -0300

    Signals experiment

--- signals.ts
+++ signals.ts
@@ -1,15 +1,49 @@
-import { createEffect, createSignal } from "solid-js/dist/solid.js";
+import { createEffect, createSignal, on } from "solid-js/dist/solid.js";
 
-import { createStore } from "solid-js/store/dist/store.js";
-const [character, setCharacter] = createStore({
-  char1: 1,
-  char2: 2,
-});
+const activeCharacters = [];
 
-createEffect(() => {
-  console.log("char1 updates", character.char1);
-});
+const newCharacter = (name: string, connectionId: number) => {
+  const char = {
+    name: createSignal(name),
+    hp: createSignal(100),
+  };
+
+  activeCharacters.push(char);
+
+  createEffect(
+    on(char.name[0], () => {
+      console.log({
+        connectionId,
+        packet: "NAME_UPDATE",
+        value: char.name[0](),
+      });
+    })
+  );
+
+  createEffect(
+    on(char.hp[0], () => {
+      console.log({
+        connectionId,
+        packet: "HP_UPDATE",
+        value: char.hp[0](),
+      });
+    })
+  );
+};
+
+const attack = (target) => {
+  target.hp[1](target.hp[0]() - 10);
+};
+
+newCharacter("Zeus", 1);
+newCharacter("Hades", 2);
 
 setInterval(() => {
-  setCharacter("char1", Date.now());
+  if (activeCharacters[1].hp[0]() > 0) {
+    attack(activeCharacters[1]);
+  }
 }, 1000);
+
+setInterval(() => {
+  activeCharacters[0].name[1]((name) => name + "!");
+}, 3000);
